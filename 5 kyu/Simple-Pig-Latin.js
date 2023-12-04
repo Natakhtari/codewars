db4e473962b880b7275d052d4f7d38f0914dc14c
vage8 <vage123580@gmail.com>
2023-12-04 20:24:50 +0400

    Simple-Pig-Latin.js

---
+++ 5 kyu/Simple-Pig-Latin.js
@@ -0,0 +1,26 @@
+// Move the first letter of each word to the end of it,
+// then add "ay" to the end of the word. Leave punctuation
+// marks untouched.
+//     Examples
+//
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+
+//SOLUTION
+
+function pigIt(str) {
+    let words = str.split(' ');
+    let result = words.map(word => {
+        if (word.match(/[a-zA-Z]/)) {
+            return word.slice(1) + word[0] + 'ay';
+        } else {
+            return word;
+        }
+    });
+
+    return result.join(' ');
+
+}
+
+console.log(pigIt('Pig latin is cool'));
